63c5c5fbd261f19dff7a91d52f9cf7c0fc7dc5ee
maoxuena <maoxuena@foxmail.com>
2022-03-02 22:25:01 +0800

    feat: tagsView 实现

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -4,15 +4,78 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+import { isTags } from '@/utils/tags'
+import { generateTitle, watchSwitchLang } from '@/utils/i18n'
+
+const route = useRoute()
+
+/**
+ * 生成 title
+ */
+const getTitle = route => {
+  let title = ''
+  if (!route.meta) {
+    // 处理无 meta 的路由
+    const pathArr = route.path.split('/')
+    title = pathArr[pathArr.length - 1]
+  } else {
+    title = generateTitle(route.meta.title)
+  }
+  return title
+}
+
+/**
+ * 监听路由变化
+ */
+const store = useStore()
+watch(
+  route,
+  (to, from) => {
+    // 并不是所有路由都需要被保存
+    if (!isTags(to.path)) return
+    const { fullPath, meta, name, params, path, query } = to
+    store.commit('app/addTagsViewList', {
+      fullPath,
+      meta,
+      name,
+      params,
+      path,
+      query,
+      title: getTitle(to)
+    })
+  },
+  {
+    immediate: true
+  }
+)
+
+/**
+ * 国际化 tags
+ */
+watchSwitchLang(() => {
+  store.getters.tagsViewList.forEach((route, index) => {
+    store.commit('app/changeTagsView', {
+      index,
+      tag: {
+        ...route,
+        title: getTitle(route)
+      }
+    })
+  })
+})
+</script>
 
 <style lang="scss" scoped>
 .app-main {
-  min-height: calc(100vh - 50px);
+  min-height: 100vh;
   width: 100%;
   position: relative;
   overflow: hidden;
-  padding: 61px 20px 20px 20px;
+  padding: 105px 20px 20px 20px;
   box-sizing: border-box;
 }
 </style>
